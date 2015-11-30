f810da657befb4e7b57eafcedb36bfa8643618dc
Jason M. Wood <sandain@hotmail.com>
2015-11-30 12:05:57 -0700

    HFS finder: output LFS if requested

--- scripts/hfs-finder.pl
+++ scripts/hfs-finder.pl
@@ -6,15 +6,15 @@ use warnings;
 use Bio::Seq;
 use Bio::SeqIO;
 
+my $usage = "Usage: $0 <input fasta> <output fasta> <HFS cutoff, optional> <LFS cutoff, optional>\n";
+
 ## Check command line argument.
-if ( @ARGV < 2 ) {
-  print "Usage: $0 <input fasta> <output fasta> <cutoff, optional>\n";
-  exit 1;
-}
+die $usage if (@ARGV < 2);
 
-my ($inputFile, $outputFile, $cutoff) = @ARGV;
+my ($inputFile, $outputFile, $hfsCutoff, $lfsCutoff) = @ARGV;
 
-$cutoff = 0 if (not defined $cutoff or $cutoff eq '');
+$hfsCutoff = 0 if (not defined $hfsCutoff or $hfsCutoff eq '');
+$lfsCutoff = ~0 if (not defined $lfsCutoff or $lfsCutoff eq '' or $hfsCutoff == 0);
 
 # Make sure the input fasta file exists.
 die "Input file not found!\n" if (! -e $inputFile);
@@ -32,25 +32,38 @@ my $outputIO = new Bio::SeqIO (
 );
 
 # Load each genotype.
-my %genotypes;
+my (@genotypes, %genotypes);
 while (my $seq = $inputIO->next_seq) {
+  # Ignore any sequences with ambiguous bases.
+  next if ($seq->seq =~ /[rywsmkbdhvnRYWSMKBDHVN]/);
+  # Add the sequence to the list of genotypes.
+  push @genotypes, $seq->seq if (not $seq->seq ~~ @genotypes);
   push @{$genotypes{$seq->seq}}, $seq;
 }
 # Close the SeqIO object.
 $inputIO->close;
 
 # Output the first representative sequence of each genotype.
-my $seqnum = 0;
-foreach my $genotype (keys %genotypes) {
-  if (@{$genotypes{$genotype}} > $cutoff) {
-    my $seq = $genotypes{$genotype}[0];
-    $seqnum ++;
-    $outputIO->write_seq (
-      new Bio::Seq (
-        -id   => 'HFS' . $seqnum,
-        -seq  => $seq->seq,
-        -desc => ''
-      )
-    );
+my $hfsNum = 0;
+my $lfsNum = 0;
+foreach my $genotype (@genotypes) {
+  my $id = 'null';
+  my $size = @{$genotypes{$genotype}};
+  my $seq = $genotypes{$genotype}[0]->seq;
+  if ($size >= $hfsCutoff) {
+    $id = sprintf "HFS%03d", $hfsNum;
+    $hfsNum ++;
   }
+  elsif ($size > $lfsCutoff) {
+    $id = sprintf "LFS%03d", $lfsNum;
+    $lfsNum ++;
+  }
+  next if ($id eq 'null');
+  $outputIO->write_seq (
+    new Bio::Seq (
+      -id   => $id,
+      -seq  => $seq,
+      -desc => $size
+    )
+  );
 }
