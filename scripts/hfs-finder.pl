1c2367c6f94a39c9694afbe0132e63ea8a642903
Jason M. Wood <sandain@hotmail.com>
2016-07-02 16:43:39 -0600

    hfs-finder: use a faster algorithm

--- scripts/hfs-finder.pl
+++ scripts/hfs-finder.pl
@@ -13,8 +13,10 @@ die $usage if (@ARGV < 2);
 
 my ($inputFile, $outputFile, $hfsCutoff, $lfsCutoff) = @ARGV;
 
-$hfsCutoff = 0 if (not defined $hfsCutoff or $hfsCutoff eq '');
-$lfsCutoff = ~0 if (not defined $lfsCutoff or $lfsCutoff eq '' or $hfsCutoff == 0);
+# A HFS cutoff of 0 is the same as a cutoff of 1.
+$hfsCutoff = 0 if (not defined $hfsCutoff);
+# Define the LFS cutoff as a really high number by default.
+$lfsCutoff = ~0 if (not defined $lfsCutoff or $hfsCutoff == 0);
 
 # Make sure the input fasta file exists.
 die "Input file not found!\n" if (! -e $inputFile);
@@ -24,38 +26,31 @@ my $inputIO = new Bio::SeqIO (
   -file   => '<' . $inputFile,
   -format => 'fasta'
 );
+my %genotypes;
+while (my $seq = $inputIO->next_seq) {
+  # Ignore any sequences with ambiguous bases.
+  next if ($seq->seq =~ /[rywsmkbdhvnRYWSMKBDHVN]/);
+  # Add the sequence to the list of genotypes.
+  $genotypes{$seq->seq} ++;
+}
+# Close the SeqIO object.
+$inputIO->close ();
 
 # Create a SeqIO object for the output fasta file.
 my $outputIO = new Bio::SeqIO (
   -file   => '>' . $outputFile,
   -format => 'fasta'
 );
-
-# Load each genotype.
-my (@genotypes, %genotypes);
-while (my $seq = $inputIO->next_seq) {
-  # Ignore any sequences with ambiguous bases.
-  next if ($seq->seq =~ /[rywsmkbdhvnRYWSMKBDHVN]/);
-  # Add the sequence to the list of genotypes.
-  push @genotypes, $seq->seq if (not $seq->seq ~~ @genotypes);
-  push @{$genotypes{$seq->seq}}, $seq;
-}
-# Close the SeqIO object.
-$inputIO->close;
-
-# Output the first representative sequence of each genotype.
 my $hfsNum = 0;
 my $lfsNum = 0;
-foreach my $genotype (@genotypes) {
+foreach my $seq (sort { $genotypes{$b} <=> $genotypes{$a} } keys %genotypes) {
   my $id = 'null';
-  my $size = @{$genotypes{$genotype}};
-  my $seq = $genotypes{$genotype}[0]->seq;
-  if ($size >= $hfsCutoff) {
-    $id = sprintf "HFS%03d", $hfsNum;
+  if ($genotypes{$seq} >= $hfsCutoff) {
+    $id = sprintf "HFS%04d", $hfsNum;
     $hfsNum ++;
   }
-  elsif ($size > $lfsCutoff) {
-    $id = sprintf "LFS%03d", $lfsNum;
+  elsif ($genotypes{$seq} >= $lfsCutoff) {
+    $id = sprintf "LFS%04d", $lfsNum;
     $lfsNum ++;
   }
   next if ($id eq 'null');
@@ -63,7 +58,8 @@ foreach my $genotype (@genotypes) {
     new Bio::Seq (
       -id   => $id,
       -seq  => $seq,
-      -desc => $size
+      -desc => $genotypes{$seq}
     )
   );
 }
+$outputIO->close ();
