02feb0a529f6be0bb5f96c34dae7ebb8419a9ae0
Jason M. Wood <sandain@hotmail.com>
2017-03-24 14:47:40 -0600

    editSVG: support more input formats

--- scripts/editSVG.pl
+++ scripts/editSVG.pl
@@ -16,45 +16,57 @@ open OUTPUT, '>' . $outputFile or die "Error: unable to write to file: $!\n";
 while (my $line = <INPUT>) {
   $line =~ s/[\r\n]//g;
 
-  if ($line =~ /(.*)translate\((\d+),(\d+)\) rotate\(-90\) translate\(-\d+,-\d+\)(.*)/) {
+  if ($line =~ /(.*)translate\(([\d.]+),([\d.]+)\) rotate\(-90\) translate\(-[\d.]+,-[\d.]+\)(.*)/) {
     my $x = $2 + $xOffset;
     my $y = $3 + $yOffset;
     $line = "$1rotate(-90 $x,$y)$4";
   }
-
-  if ($line =~ /<text\s+x="(\d+)"\s+y="(\d+)"(.*)/) {
-    my $x = $1 + $xOffset;
-    my $y = $2 + $yOffset;
-    print OUTPUT "  <text x=\"$x\" y=\"$y\"$3\n";
+  if ($line =~ /(.*)rotate\(-90 ([\d.]+),([\d.]+)\)(.*)/) {
+    my $x = $2 + $xOffset;
+    my $y = $3 + $yOffset;
+    $line = "$1rotate(-90 $x,$y)$4";
   }
-  elsif ($line =~ /(.*)<tspan\s+(.*)x=\"(\d+)\"\s+y=\"(\d+)\"(.*)/) {
+  if ($line =~ /(.*)<text\s+x="([\d.]+)"\s+y="([\d.]+)"(.*)/) {
+    my $x = $2 + $xOffset;
+    my $y = $3 + $yOffset;
+    print OUTPUT "$1<text x=\"$x\" y=\"$y\"$4\n";
+  }
+  elsif ($line =~ /(.*)<tspan\s+(.*)x=\"([\d.]+)\"\s+y=\"([\d.]+)\"(.*)/) {
     my $x = $3 + $xOffset;
     my $y = $4 + $yOffset;
     print OUTPUT "$1<tspan $2x=\"$x\" y=\"$y\"$5\n";
   }
-  elsif ($line =~ /<line\s+x1="(\d+)"\s+y1="(\d+)"\s+x2="(\d+)"\s+y2="(\d+)"(.*)/) {
-    my $x1 = $1 + $xOffset;
-    my $y1 = $2 + $yOffset;
-    my $x2 = $3 + $xOffset;
-    my $y2 = $4 + $yOffset;
-    print OUTPUT "  <line x1=\"$x1\" y1=\"$y1\" x2=\"$x2\" y2=\"$y2\"$5\n";
+  elsif ($line =~ /(.*)<line\s+x1="([\d.]+)"\s+y1="([\d.]+)"\s+x2="([\d.]+)"\s+y2="([\d.]+)"(.*)/) {
+    my $x1 = $2 + $xOffset;
+    my $y1 = $3 + $yOffset;
+    my $x2 = $4 + $xOffset;
+    my $y2 = $5 + $yOffset;
+    print OUTPUT "$1<line x1=\"$x1\" y1=\"$y1\" x2=\"$x2\" y2=\"$y2\"$6\n";
   }
-  elsif ($line =~ /<path\s+(.*)d=\"(.*)\"(.*)\/>/) {
+  elsif ($line =~ /(.*)<path\s+(.*)d=\"([\w\s\,\-]+)\"(.*)\/>/) {
     my $path = "";
-    foreach my $cmd (split /\s+/, $2) {
-      if ($cmd =~ /[ML]/) {
-        $path .= $cmd . ' ';
-      }
-      elsif ($cmd =~ /[Z]/) {
+    foreach my $cmd (split /\s+/, $3) {
+      if ($cmd =~ /[Z]/i) {
         $path .= $cmd;
       }
-      elsif ($cmd =~ /(\d+),(\d+)/) {
+      elsif ($cmd =~ /([\d.]+),([\d.]+)/) {
         my $x = $1 + $xOffset;
         my $y = $2 + $yOffset;
         $path .= $x . ',' . $y . ' ';
       }
+      else {
+        $path .= $cmd . ' ';
+      }
     }
-    print OUTPUT "  <path $1d=\"$path\"$3\/>\n";
+    print OUTPUT "$1<path $2d=\"$path\"$4\/>\n";
+  }
+  elsif ($line =~ /(.*)<rect\s+(.*)x=\"([\d.]+)\"\s+y=\"([\d.]+)\"\s+width=\"([\d.]+)\"\s+height=\"([\d.]+)\"(.*)/) {
+    my $x1 = $3 + $xOffset;
+    my $y1 = $4 + $yOffset;
+    my $x2 = $x1 + $5;
+    my $y2 = $y1 + $6;
+    my $path = "M $x1,$y1 L $x2,$y1 $x2,$y2 $x1,$y2 Z";
+    print OUTPUT "$1<path $2d=\"$path\"$7\n";
   }
   else {
     print OUTPUT "$line\n";
