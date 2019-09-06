547560e1d44e9049f4dbad99a48d7c79d9889cc6
Jason M. Wood <sandain@hotmail.com>
2019-09-05 17:15:59 -0700

    slice-sequence: make arguments optional

--- scripts/slice-sequence.pl
+++ scripts/slice-sequence.pl
@@ -6,13 +6,16 @@ use warnings;
 use Bio::SeqIO;
 
 
-my $usage = "Usage: $0 <Input file> <Sequence Identifiers> <Start> <Stop> <Strand>\n";
+my $usage = "Usage: $0 <Input file> <Sequence Identifier> <Start> <Stop> <Strand>\n";
 
-die $usage unless (@ARGV == 5);
+die $usage unless (@ARGV >= 2);
 
-my ($inputFile, $identifiers, $start, $stop, $strand) = @ARGV;
+my ($inputFile, $identifier, $start, $stop, $strand) = @ARGV;
 
 die $usage unless (-e $inputFile);
+
+$start = 1 unless (defined $start);
+$strand = 1 unless (defined $strand);
 
 my $format = 'fasta';
 $format = 'fastq' if ($inputFile =~ /fastq/i);
@@ -22,10 +25,10 @@ my $seqIO = new Bio::SeqIO (
   -format => $format
 );
 
-my %identifiers = map { $_ => 1 } split /\s+/, $identifiers;
 
 while (my $seq = $seqIO->next_seq) {
-  next unless (defined $identifiers{$seq->id});
+  next unless ($seq->id =~ /$identifier/g);
+  $stop = $seq->length unless (defined $stop);
   my $sequence = substr $seq->seq, $start, ($stop - $start);
   $sequence = reverse complement ($sequence) if ($strand == -1);
   print '>' . $seq->id;
